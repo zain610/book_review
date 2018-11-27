22a52a8b5644f57356436147c11d3b7934eabb29
zain610 <znshroff@gmail.com>
2018-11-27 13:37:06 +0530

    Working on Search bar. Successfully completed search for title, isbn, author data from database To Do:  - formatting data to iterate and instead of printing out in 1 line.  - Do ISBN and Author tables
    Signed-off-by: zain610 <znshroff@gmail.com>

--- application.py
+++ application.py
@@ -31,14 +31,14 @@ def index():
     :return: Index.html and process data to be showed on this page.
     '''
     isbn = []
-
-    res = requests.get("https://www.goodreads.com/book/review_counts.json",params={"key": "jUV1zj5KRLBJxzNzllbvQw", "isbns": [isbn]})
+    res = requests.get("https://www.goodreads.com/book/review_counts.json",params={"key": "jUV1zj5KRLBJxzNzllbvQw", "isbns": "0441172717,0141439602"})
+    print(res)
     try:
         username=session['username']
+        print(username)
+        return render_template("index.html", data=res.json(), message=("Hello "+ username ))
     except KeyError:
         return render_template("index.html", data=res.json(), message=("Hello, Please Login!"))
-    return render_template("index.html", data = res.json(), message=("Hello "+ username))
-
 
 
 @app.route("/register", methods=["POST", "GET"])
@@ -98,9 +98,44 @@ def login():
             return render_template('login.html')
         except ValueError:
             return render_template("error.html", message="Invalid credentials provided")
+    return render_template('login.html')
 
+
+#
 @app.route("/search", methods=["POST", "GET"])
 def search():
+    '''
+    Search page
+    Take title, isbn, author name and output resutls
+
+    :return:
+    '''
+    if request.method == "POST":
+        keyword = str(request.form.get('keyword'))
+        keyword = keyword.title()
+        word = "%" + keyword + "%"
+        print(word)
+        # get title data
+        title_data = db.execute(
+            "Select * from book where title like (:keyword)",
+            {"keyword": word}
+        ).fetchall()
+        print(title_data)
+        # get isbn data
+        isbn_data = db.execute(
+            "Select * from book where isbn like (:isbn)",
+            {"isbn": word}
+        ).fetchall()
+        print(isbn_data)
+        # get author data
+        author_data = db.execute(
+            "Select * from book where author like (:author)",
+            {"author": word}
+        ).fetchall()
+        print(author_data)
+        return render_template("search.html", title_data = title_data)
+    return render_template("search.html")
+
 
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
